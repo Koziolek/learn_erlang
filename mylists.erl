776bb837ebd8de0dff45b8a04e052224fda971b1
Koziolek <bjkuczynski@gmail.com>
2014-09-15 22:57:40 +0200

    first blood

---
+++ mylists.erl
@@ -0,0 +1,19 @@
+%%%-------------------------------------------------------------------
+%%% @author koziolek
+%%% @copyright (C) 2014, <COMPANY>
+%%% @doc  few simple exercises with lists
+%%%
+%%% @end
+%%% Created : 15. wrz 2014 22:22
+%%%-------------------------------------------------------------------
+-module(mylists).
+-author("koziolek").
+
+%% API
+-export([simpleList/0]).
+
+simpleList() ->
+  L = [1, 2, 3],
+  lists:foreach(show, L);
+
+show(X) -> io:format("So what ~p ~n", [X]) end.
